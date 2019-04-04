8aae2dfe1a28b0d0cf1fce49619118e0dc9805e4
Rene Cocom <fasnimo@gmail.com>
2019-04-04 05:18:17 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -4,12 +4,6 @@ def square_array(array)
   new_array = []
   array.each do |num|
   s = Math.sqrt(num)
-  if (s - s.floor).zero?
-    new_array << s.to_i 
-  else 
-  new_array << num * num 
-end
- puts new_array
 end
 end
 
