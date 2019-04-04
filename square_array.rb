b360840e1583ef106b0f7c32985487037026c0df
Rene Cocom <fasnimo@gmail.com>
2019-04-04 05:05:00 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,16 @@
-def square_array(array)
-  # your code here
+
+
+def square_array(numbers)
+  
+numbers = [1, 4, 9]
+
+
+
+ 
 end
+
+def square_array(new_numbers)
+  new_numbers = [81, 100, 256, 625]
+ 
+ 
+ end
