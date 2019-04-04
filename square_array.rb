e6eef4b830ac222c6ccdd1010f27bf193fcd7fa0
Rene Cocom <fasnimo@gmail.com>
2019-04-04 05:25:18 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,9 @@
 
 
-def square_array(new_array)
-  numbers = []
+def square_array(new_numbers)
+  arr = []
   
-  new_array.each do |num|
+  new_numbers.each do |num|
   s = Math.sqrt(num)
 end
 end
